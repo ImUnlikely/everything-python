5917bbd601506871602474dd40c0e25ebbd9115d
Thomas Hennø <thomas.hennoo@hotmail.com>
2020-06-05 23:49:58 +0200

    Re-added all files and folders for folder update test

---
+++ generators/converting_generator_to_list.py
@@ -0,0 +1,8 @@
+from basic_square_numbers_generator import square_numbers
+
+# Define numbers
+nums = [1,2,3,4,5]
+
+# Making list of generator
+my_nums = list(square_numbers(nums)) 
+print(my_nums)
