18a78020fa5a6c8b473ccc3a15b26bbff96ae667
Thomas Hennø <thomas.hennoe@hotmail.com>
2020-08-19 14:46:11 +0200

    Update and.py
    test commit from new pc

---
+++ keywords/and.py
@@ -0,0 +1,28 @@
+x = False
+y = True
+
+print("Round 1")
+if x and y: # Both conditions must be true
+    print("Both True")
+else:
+    print("Both not True")
+
+
+x = True
+y = True
+
+print("\nRound 2")
+if x and y: # Both conditions must be True
+    print("Both True")
+else:
+    print("Both not True")
+
+
+x = False
+y = False
+
+print("\nRound 3")
+if x and y: # Both conditions must be True
+    print("Both True")
+else:
+    print("Both not True")
